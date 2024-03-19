a7f38575c2d1a6b67bd157d3ab5beb710659d066
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-19 15:30:23 -0700

    $

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,15 +9,15 @@ env.hosts = ["104.196.168.90", "35.196.46.172"]
 def do_clean(number=0):
     """delete out of date archeives 
     """
-    arvnum = 1 if int(arvnum) == 0 else int(arvnum)
+    arvnum = 1 if int(number) == 0 else int(number)
 
     archeive = sorted(os.listdir("versions"))
-    [archeive.pop() for i in range(arvnum)]
+    [archeive.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archeive]
 
     with cd("/data/web_static/releases"):
         archeive = run("ls -tr").split()
         archeive = [a for a in archeive if "web_static_" in a]
-        [archeive.pop() for i in range(arvnum)]
+        [archeive.pop() for i in range(num)]
         [run("rm -rf ./{}".format(a)) for a in archeive]
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,43 +8,52 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dicationary = {}
+        if cls:
+            dic = self.__objects
+            for k in dic:
+                part = k.replace('.', ' ')
+                part = shelx.split(part)
+                if (part[0] == cls.__name__):
+                    dicationary[k] = self.__objects[k]
+            return (dicationary)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        if obj:
+            k = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[k] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        dictt = {}
+        for k, v in self.__objects.items():
+            dictt[k] = v.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(dictt, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for k, value in
+(json.load(f)).items():
+                    value = eval(value["__class__"])
+(**value)
+                    self.__objects[k] = value 
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete"""
+        if obj:
+            k = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[k]
+
+    def close(self):
+        """ calls"""
+        self.reload()
