ed877769c388855d2f5633be8413fc5fab5679e4
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-20 18:16:29 -0700

    $

--- models/city.py
+++ models/city.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""city class"""
-import models
+"""city calss"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """city class"""
-    # initialize class for file/db storage type
-    __tablename__ = 'cities'
+    """city class
+    """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete, delete-orphan', backref='cities')
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
