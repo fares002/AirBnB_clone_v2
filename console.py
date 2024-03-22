6b5f2bd4f0bd4f2ec42646c648ce3b4fad96c7f6
Abeermohsien <aboorakhalil993@gmail.com>
2024-03-22 13:15:08 -0700

    $

--- console.py
+++ console.py
@@ -120,19 +120,19 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not args:
                 raise SyntaxError()
-            arg_list = args.split(" ")
-            kw = {}
-            for arg in arg_list[1:]:
+            my_list = args.split(" ")
+            kwargs = {}
+            for arg in my_list[1:]:
                 arg_splited= arg.split("=")
                 arg_splited[1] = eval(arg_splited[1])
                 if type(arg_splited[1]) is str:
                     arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
-                kw[arg_splited[0]] = arg_splited[1]
+                kwargs[arg_splited[0]] = arg_splited[1]
         except SyntaxError:
             print("** class name missing **")
         except NameError:
             print("** class doesn't exist **")
-        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance = HBNBCommand.classes[my_list[0]](**kwargs)
         new_instance.save()
         print(new_instance.id)
 
