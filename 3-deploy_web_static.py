f497bf014a8faf578b88b8aea77926032aa75b31
Abeermohsien <aboorakhalil993@gmail.com>
2024-04-04 06:59:17 -0700

    $

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -15,30 +15,30 @@ def do_deploy(archive_path):
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
-    splited = file.split(".")[0]
+    splt = file.split(".")[0]
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(splited)).failed is True:
+           format(splt)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(splited)).failed is True:
+           format(splt)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, splited)).failed is True:
+           format(file, splt)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, splited)).failed is True:
+           "/data/web_static/releases/{}/".format(splt, splt)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(splited)).failed is True:
+           format(splt)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(splited)).failed is True:
+           format(splt)).failed is True:
         return False
     return True
