7df8011063aa9c6e81a257af407b23f999e06694
fares <fares002@example.com>
2024-04-20 10:50:49 -0700

    l

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,14 +6,15 @@ import os
 env.hosts = ['54.152.134.222', '35.168.2.251']
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     """
-    This function is to deploy 
+    This function is to deploy
     """
 
     if not os.path.exists(archive_path):
-           print("Error: Archive file {} does not exist.".format(archive_path))
-           return False
+        print("Error: Archive file {} does not exist.".format(archive_path))
+        return False
 
     filename = os.path.basename(archive_path)
     folder_name = filename.split('.')[0]
@@ -23,7 +24,8 @@ def do_deploy(archive_path):
     run('mkdir -p {}'.format(release_path))
     run('tar -xzf {} -C {}'.format(tmp, release_path))
     run('rm  /tmp/{}'.format(filename))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
+    run('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/'.format(folder_name, folder_name))
     # Delete the archive from the web server
     run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
     # Delete the symbolic link /data/web_static/current
